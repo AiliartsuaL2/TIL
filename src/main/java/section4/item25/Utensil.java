28d04767abb9e62e6f9ed78257b8af8ee967a6bb
ailiartsua <ailiartsual2@gmail.com>
2023-12-30 20:58:16 +0900

    아이템 25 : 톱 레벨 클래스는 한 파일에 하나만 담아라 라이브 코딩

---
+++ src/main/java/section4/item25/Utensil.java
@@ -0,0 +1,10 @@
+package section4.item25;
+
+// 클래스가 한 파일에 정의가 되어있음 -> 절대 따라하지 말 것
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
