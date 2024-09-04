9c624c40cec2044b18b6ffb430d19c70578e48aa
ailiartsua <ailiartsual2@gmail.com>
2024-09-04 21:09:29 +0900

    섹션 14: ExecutorService 종료 및 스레드 풀 관리 라이브 코딩

--- src/thread/executor/ExecutorUtils.java
+++ src/thread/executor/ExecutorUtils.java
@@ -27,4 +27,25 @@ public abstract class ExecutorUtils {
             log(executorService);
         }
     }
+
+    public static void printState(ExecutorService executorService, String taskName) {
+        // ThreadPoolExecutor로 캐스팅
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            // 현재 생성된 스레드의 개수가 몇개인지
+            int pool = poolExecutor.getPoolSize();
+
+            // 실제 작업을 하고있는 스레드의 개수가 몇개인지
+            int active = poolExecutor.getActiveCount();
+
+            // 작업이 몇개 들어가있는지
+            int queuedTasks = poolExecutor.getQueue().size();
+
+            // 작업이 완료된 스레드가 몇개인지
+            long completedTaskCount = poolExecutor.getCompletedTaskCount();
+
+            log(taskName + " -> [pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTaskCount=" + completedTaskCount + "]");
+        } else {
+            log(executorService);
+        }
+    }
 }
